927c3961adf4a4d2fa78e37cd15e666f60f2e639
Eduardo Diaz Huayhuas <ediaz100pre@hotmail.com>
2023-06-11 19:31:11 -0500

    Update readme.ml

--- readme.ml
+++ readme.ml
@@ -1,2 +1,21 @@
-##Universidad Nacional de San Agustin
-###Maestria en Ciencias de la Computación
+## Universidad Nacional de San Agustín <br/> Facultad de Producción  y Servicios<br/>  Maestría en Ciencia de la Computación 
+<hr/>
+
+
+## Proyecto final de Curso
+
+| DOCENTE DEL CURSO | MAESTRIA | CURSO |
+| :-: | :-: | :-: |
+| MSc. Vicente Machaca Arceda | Maestría en Ciencia de la Computación | Algoritmos y Estructura de Datos |
+<br/>
+
+## Tema:
+
+Aplicacion del algoritmo KDTree en la lectura de imagenes DICOM para detección de patologias en pulmones.
+
+## Integrantes:
+
+- Angel Yvan Choquehuanca Peraltilla
+- Estefany Pilar Huaman Colque
+- Eduardo Diaz Huayhuas
+- Gustavo Raul Manrique Fernandez
